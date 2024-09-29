fe666e58a6ed73b043ade6e71bacd4b819449dbd
Christopher Caperlac <157346675+ChristopherCaperlac@users.noreply.github.com>
2024-09-28 22:59:13 -0700

    Delete Project 1: Hash Function directory

--- Project 1: Hash Function/hash.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-// You are free to use additional libraries as long as it's not PROHIBITED per instruction.
-
-using namespace std;
-
-int hash_function(string text);
